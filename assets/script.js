85b8f8eafbd533714e5ae8e9d122fa67e8323386
Beth Allaway <beth_allaway@yahoo.com>
2022-11-03 22:42:13 +0000

    display random food options

--- assets/script.js
+++ assets/script.js
@@ -15,14 +15,19 @@ const completeMeals = ['Pasta Bolognase', 'Sweet n Sour Chicken', 'Sweet n Sour 
 'Feng Shui Pork', 'Pizza', 'Chicken Tikka Masala', 'Butter Chicken', 'Pringle Chicken',
 'Pasta Carbonara', 'Stew n Dumplings', 'Tinned Spag Bowl on Toast', 'English Breakfast']
 
-const getRandomNumber
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
-const getMain = () => `${mainFood[Math.floor(Math.random() * mainFood.length)]}`;
-const getSecond = () => `${secondaryFood[Math.floor(Math.random() * secondaryFood.length)]}`;
-const getThird = () => `${tertiaryFood[Math.floor(Math.random() * tertiaryFood.length)]}`;
-const getMeal = () => `${completeMeals[Math.floor(Math.random() * completeMeals.length)]}`;
+const getMain = () => `${mainFood[getRandomNumber(mainFood.length)]}`;
+const getSecond = () => `${secondaryFood[getRandomNumber(secondaryFood.length)]}`;
+const getThird = () => `${tertiaryFood[getRandomNumber(tertiaryFood.length)]}`;
+const getMeal = () => `${completeMeals[getRandomNumber(completeMeals.length)]}`;
 
 console.log(getMain())
 console.log(getSecond())
 console.log(getThird())
 console.log(getMeal())
+
+document.getElementById('main-food').innerText = getMain();
+document.getElementById('second-food').innerText = getSecond();
+document.getElementById('third-food').innerText = getThird();
+document.getElementById('comp-meal').innerText = getMeal();
